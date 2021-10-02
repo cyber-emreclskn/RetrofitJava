3b9e1cb8a15442ebd32de3ffeaaef8e0db9c59e3
cyber-emreclskn <emre_2001.osman@hotmail.com>
2021-10-02 16:46:44 +0300

    v2c

--- app/src/main/java/com/example/retrofitjava/view/MainActivity.java
+++ app/src/main/java/com/example/retrofitjava/view/MainActivity.java
@@ -24,6 +24,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
 
